8ea380134aa43803138c7ca90391dd8d4f9f736b
Cewa Mahan <cewa.mahan@student.moringaschool.com>
2026-01-03 22:57:12 +0300

    Refactor test setup for JSDOM and fetch handling

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,31 +6,25 @@ const path = require('path');
 const { JSDOM } = require('jsdom');
 const babel = require('@babel/core');
 
-// Load HTML content
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
-// Transform JavaScript using Babel
 const { code: transformedScript } = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'),
   { presets: ['@babel/preset-env'] }
 );
 
-// Initialize JSDOM
 const dom = new JSDOM(html, {
   runScripts: "dangerously",
   resources: "usable"
 });
 
-//Handle fetch
 const fetchPkg = 'node_modules/whatwg-fetch/dist/fetch.umd.js';
 dom.window.eval(fs.readFileSync(fetchPkg, 'utf-8'));
 
-// Inject the transformed JavaScript into the virtual DOM
 const scriptElement = dom.window.document.createElement("script");
 scriptElement.textContent = transformedScript;
 dom.window.document.body.appendChild(scriptElement);
 
-// Expose JSDOM globals to the testing environment
 global.window = dom.window;
 global.document = dom.window.document;
 global.navigator = dom.window.navigator;
@@ -39,7 +33,6 @@ global.Node = dom.window.Node;
 global.Text = dom.window.Text;
 global.XMLHttpRequest = dom.window.XMLHttpRequest;
 
-// Sample test suite for JavaScript event handling
 describe('Asynchronous Fetching ', () => {
   it('should fetch to external api and add information to page', async() => {
     await new Promise(resolve => setTimeout(resolve, 1000)); 
